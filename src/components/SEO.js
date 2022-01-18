5fe2299e46046036d118305d178fc6c3b830d28a
satyajeet khanna <satyajeet_khanna@persistent.com>
2022-01-18 14:41:01 +0530

    seo update

---
+++ src/components/SEO.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+const SEO = ({ title, description }) => {
+  return <Helmet title={title}></Helmet>
+}
+
+export default SEO;
